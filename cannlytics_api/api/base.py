a6ff16d48f2aa96a4797debefb6c9ae5481adf4f
Michael Pilosov <consistentbayes@gmail.com>
2021-08-03 03:47:04 +0000

    sed -i "s|\"|\'|g" cannlytics_api/api/*.py

--- cannlytics_api/api/base.py
+++ cannlytics_api/api/base.py
@@ -1,24 +1,24 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
-BASE = "https://api.cannlytics.com"
-ENDPOINTS = ["labs"]
-VERSION = "v1"
+BASE = 'https://api.cannlytics.com'
+ENDPOINTS = ['labs']
+VERSION = 'v1'
 
 
-@api_view(["GET"])
+@api_view(['GET'])
 def index(request, format=None):
-    """Informational base endpoint."""
-    message = "Welcome to the Cannlytics API."
-    message += f"The current version is {VERSION} and is located at {BASE}/{VERSION}."
-    return Response({"data": message}, content_type="application/json")
+    '''Informational base endpoint.'''
+    message = 'Welcome to the Cannlytics API.'
+    message += f'The current version is {VERSION} and is located at {BASE}/{VERSION}.'
+    return Response({'data': message}, content_type='application/json')
 
 
-@api_view(["GET"])
+@api_view(['GET'])
 def base(request, format=None):
-    """Informational version endpoint."""
-    message = f"Welcome to {VERSION} of the Cannlytics API."
-    message += "Available endpoints:\n\n"
+    '''Informational version endpoint.'''
+    message = f'Welcome to {VERSION} of the Cannlytics API.'
+    message += 'Available endpoints:\n\n'
     for endpoint in ENDPOINTS:
-        message += f"{endpoint}\n"
-    return Response({"data": message}, content_type="application/json")
+        message += f'{endpoint}\n'
+    return Response({'data': message}, content_type='application/json')
--- cannlytics_api/api/users.py
+++ cannlytics_api/api/users.py
@@ -8,37 +8,37 @@ from rest_framework.response import Response
 # from utils.firebase import get_document, update_document
 
 
-@api_view(["GET", "POST"])
+@api_view(['GET', 'POST'])
 def users(request, format=None):
-    """Get, update, or create users."""
+    '''Get, update, or create users.'''
     # try:
     #     claims = authenticate(request)
     # except:
-    #     return Response({"error": "Could not authenticate."},
+    #     return Response({'error': 'Could not authenticate.'},
     #                     status=status.HTTP_400_BAD_REQUEST)
 
     # Get user(s).
-    if request.method == "GET":
-        print("Getting user...")
-        # limit = request.query_params.get("limit", None)
-        # order_by = request.query_params.get("order_by", "state")
+    if request.method == 'GET':
+        print('Getting user...')
+        # limit = request.query_params.get('limit', None)
+        # order_by = request.query_params.get('order_by', 'state')
         # # TODO: Get any filters from dict(request.query_params)
         # labs = get_collection('labs', order_by=order_by, limit=limit, filters=[])
         user = {}
-        return Response({"data": user}, content_type="application/json")
+        return Response({'data': user}, content_type='application/json')
 
     # Update or create user(s).
-    elif request.method == "POST":
+    elif request.method == 'POST':
         # TODO: Check if user already exists.
         # get_document
-        print("Creating a user...")
+        print('Creating a user...')
         timestamp = datetime.now().isoformat()
-        email = request.query_params.get("email", "")
+        email = request.query_params.get('email', '')
         user = {
-            "email": email,
-            "created_at": timestamp,
-            # "uid": user.uid,
-            "photo_url": f"https://robohash.org/${email}?set=set5",
+            'email': email,
+            'created_at': timestamp,
+            # 'uid': user.uid,
+            'photo_url': f'https://robohash.org/${email}?set=set5',
         }
         print(user)
-        return Response({"success": True}, content_type="application/json")
+        return Response({'success': True}, content_type='application/json')
